a62f8ee77ef1b149696ed28347bf942c0d15fd03
Sixto Ortega <ortegasixto7@gmail.com>
2021-08-13 20:45:13 -0400

    Adding logs

--- index.js
+++ index.js
@@ -18,15 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 // simple route
 app.post("/notify", async (req, res) => {
   try {
+    console.log('req_body', JSON.stringify(req.body));
     await firestore.collection('users').doc(req.body.id).set({ ...req.body });
     res.json({ message: "success" });
   } catch (error) {
+    console.log('error', JSON.stringify(error));
     res.status(500).json({ message: error });
   }
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
