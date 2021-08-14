89da943ed63d123ba5c782cb6de0957e06aead55
Sixto Ortega <ortegasixto7@gmail.com>
2021-08-13 20:47:13 -0400

    Changing port number

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/notify", async (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
