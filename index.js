6a6eae6c20e88d87b1c51a297969fb2307030e99
Sixto Ortega <ortegasixto7@gmail.com>
2021-08-15 15:50:55 -0400

    Adding refactor

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/notify', async (req, res) => {
       await firestore
         .collection('users')
         .doc(req.body.id)
-        .set({ ...result, ...req.body });
+        .update({ ...result, ...req.body });
     }
 
     res.json({ message: 'success' });
